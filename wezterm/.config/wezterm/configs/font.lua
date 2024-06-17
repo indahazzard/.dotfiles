189a384259c547f6e75247a525e0c40fb3a3c8b8
dotsenkom <mykyta.dotsenko@gimo.co.uk>
2024-06-17 10:02:45 +0300

    update fonts removed

--- wezterm/.config/wezterm/configs/font.lua
+++ wezterm/.config/wezterm/configs/font.lua
@@ -1,7 +1,25 @@
 local wezterm = require 'wezterm'
 
+local is_linux = function()
+	return wezterm.target_triple:find("linux") ~= nil
+end
+
+local is_darwin = function()
+	return wezterm.target_triple:find("darwin") ~= nil
+end
+
+local dynamic_font_size = nil
+
+if is_linux() then
+	dynamic_font_size = 16.0
+end
+
+if is_darwin() then
+	dynamic_font_size = 18.0
+end
+
 -- font
 return {
 	font = wezterm.font('Fira Code'),
-	font_size = 18.0
+	font_size = dynamic_font_size
 }
