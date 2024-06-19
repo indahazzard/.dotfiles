d150a9321048cacb9566678aa604564dbc0fb306
dotsenkom <mykyta.dotsenko@gimo.co.uk>
2024-06-19 17:58:46 +0300

    remove padding setting for wezterm

--- wezterm/.config/wezterm/configs/settings.lua
+++ wezterm/.config/wezterm/configs/settings.lua
@@ -1,10 +1,4 @@
 return {
-	hide_tab_bar_if_only_one_tab = true,
-	use_fancy_tab_bar = false,
-	window_padding = {
-		left = 2,
-		right = 2,
-		top = 0,
-		bottom = 0,
-	}
+  hide_tab_bar_if_only_one_tab = true,
+  use_fancy_tab_bar = false
 }
