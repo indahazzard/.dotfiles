6ef55eb4776796eb309b90aaa4b3218507479df5
Mykyta Dotsenko <indahazzard@gmail.com>
2024-09-22 21:12:00 +0300

    update theme

--- nvim/.config/nvim/lua/custom/plugins/theme.lua
+++ nvim/.config/nvim/lua/custom/plugins/theme.lua
@@ -6,10 +6,26 @@ return {
     priority = 1000,
     config = function()
         require("nightfox").setup({
-	  options = {
-	    transparent = true
-	  }
+            options = {
+                transparent = false
+            },
+            specs = {
+                -- Set NeoTree background color to match Normal background
+                NeoTreeNormal = {
+                    fg = "fg1",
+                    bg = "bg0" -- Uses the same background as 'Normal'
+                },
+            }
         })
-        vim.cmd("colorscheme carbonfox") -- set the colorscheme
+
+        -- Apply the colorscheme
+        vim.cmd("colorscheme carbonfox")
+
+        -- Sync NeoTree with Normal background using the new API
+        local normal_bg = vim.api.nvim_get_hl(0, { name = "Normal" }).bg
+        vim.api.nvim_set_hl(0, "NeoTreeNormal", { bg = normal_bg })
+
+        -- Remove color from NeoTree border
+vim.api.nvim_set_hl(0, "NormalFloat", {bg="NONE"})
     end,
 }
