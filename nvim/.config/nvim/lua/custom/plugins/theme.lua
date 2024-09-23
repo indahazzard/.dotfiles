660bc870606fd75a900d7d9118093bad294cfb90
Mykyta Dotsenko <indahazzard@gmail.com>
2024-09-23 23:33:34 +0300

    update conf

--- nvim/.config/nvim/lua/custom/plugins/theme.lua
+++ nvim/.config/nvim/lua/custom/plugins/theme.lua
@@ -10,10 +10,9 @@ return {
                 transparent = false
             },
             specs = {
-                -- Set NeoTree background color to match Normal background
                 NeoTreeNormal = {
                     fg = "fg1",
-                    bg = "bg0" -- Uses the same background as 'Normal'
+                    bg = "bg0"
                 },
             }
         })
@@ -24,8 +23,9 @@ return {
         -- Sync NeoTree with Normal background using the new API
         local normal_bg = vim.api.nvim_get_hl(0, { name = "Normal" }).bg
         vim.api.nvim_set_hl(0, "NeoTreeNormal", { bg = normal_bg })
+        vim.api.nvim_set_hl(0, "NotifyBackground", { bg = '#000000' })
 
         -- Remove color from NeoTree border
-vim.api.nvim_set_hl(0, "NormalFloat", {bg="NONE"})
+        vim.api.nvim_set_hl(0, "NormalFloat", {bg="NONE"})
     end,
 }
