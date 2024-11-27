e81a432518df1365d5ed6927c6abfe7e51056382
Mykyta Dotsenko <mykyta.dotsenko@danads.se>
2024-11-27 10:12:48 +0200

    revert theme

--- nvim/.config/nvim/lua/custom/plugins/theme.lua
+++ nvim/.config/nvim/lua/custom/plugins/theme.lua
@@ -1,15 +1,31 @@
 -- File: lua/custom/plugins/theme.lua
 
 return {
-    "aliqyan-21/darkvoid.nvim",
+    "EdenEast/nightfox.nvim",
     lazy = false,
     priority = 1000,
     config = function()
-        require("darkvoid").setup({
-            transparent = false,
-            glow = false
+        require("nightfox").setup({
+            options = {
+                transparent = false
+            },
+            specs = {
+                NeoTreeNormal = {
+                    fg = "fg1",
+                    bg = "bg0"
+                },
+            }
         })
 
-        vim.cmd("colorscheme darkvoid")
-    end
+        -- Apply the colorscheme
+        vim.cmd("colorscheme carbonfox")
+
+        -- Sync NeoTree with Normal background using the new API
+        local normal_bg = vim.api.nvim_get_hl(0, { name = "Normal" }).bg
+        vim.api.nvim_set_hl(0, "NeoTreeNormal", { bg = normal_bg })
+        vim.api.nvim_set_hl(0, "NotifyBackground", { bg = '#000000' })
+
+        -- Remove color from NeoTree border
+        vim.api.nvim_set_hl(0, "NormalFloat", {bg="NONE"})
+    end,
 }
