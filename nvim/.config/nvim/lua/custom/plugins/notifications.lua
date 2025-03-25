f8e668dd6777f9acfe01460f3f92fa42b9ba2cf9
Mykyta Dotsenko <mykyta.dotsenko@danads.se>
2025-03-25 22:19:53 +0200

    fix notifications warning

--- nvim/.config/nvim/lua/custom/plugins/notifications.lua
+++ nvim/.config/nvim/lua/custom/plugins/notifications.lua
@@ -1,6 +1,10 @@
 return {
   'rcarriga/nvim-notify',
   config = function()
-      vim.notify = require("notify")
+      require("notify").setup({
+          background_colour = "#000000"
+      })
+
+      vim.notify = require('notify')
   end,
 }
