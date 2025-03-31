35abef33df74751ff843da8e231ba1ae3e371a80
Mykyta Dotsenko <mykyta.dotsenko@danads.se>
2025-04-01 00:09:28 +0300

    update conf

---
+++ nvim/.config/nvim/lua/custom/plugins/blink.lua
@@ -0,0 +1,54 @@
+return {
+  'saghen/blink.cmp',
+  dependencies = { 'rafamadriz/friendly-snippets' },
+  version = '1.*',
+
+  opts = {
+    keymap = {
+      preset = 'super-tab',
+      ['<C-n>'] = { 'select_next', 'fallback' },
+      ['<C-p>'] = { 'select_prev', 'fallback' },
+      ['<C-d>'] = { function(cmp) cmp.scroll_documentation_down(4) end },
+      ['<C-f>'] = { function(cmp) cmp.scroll_documentation_up(4) end },
+      ['<C-Space>'] = { 'show', 'fallback' },
+      ['<CR>'] = { 'accept', 'hide' },
+      ['<Tab>'] = {
+        'snippet_forward',
+        'select_next',
+        'fallback'
+      },
+      ['<S-Tab>'] = {
+        'snippet_backward',
+        'select_prev',
+        'fallback'
+      }
+    },
+
+    completion = {
+      documentation = {
+        auto_show = true,
+        window = {
+            border = 'rounded'
+        }
+      },
+
+      menu = {
+          border = 'rounded'
+      }
+    },
+
+    sources = {
+      default = {
+        'lsp',
+        'path',
+        'snippets',
+        'buffer'
+      },
+    },
+
+    fuzzy = {
+      implementation = "prefer_rust_with_warning"
+    }
+  },
+  opts_extend = { "sources.default" }
+}
--- wezterm/.config/wezterm/configs/settings.lua
+++
@@ -1,4 +0,0 @@
-return {
-	hide_tab_bar_if_only_one_tab = false,
-	use_fancy_tab_bar = false,
-}
