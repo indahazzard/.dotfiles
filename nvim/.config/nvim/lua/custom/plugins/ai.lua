da76550ebcf5b99d9d5d29d88585205721c5a616
Mykyta Dotsenko <mykyta.dotsenko@danads.se>
2025-04-22 23:59:55 +0300

    ai works

--- nvim/.config/nvim/lua/custom/plugins/ai.lua
+++ nvim/.config/nvim/lua/custom/plugins/ai.lua
@@ -1,76 +1,46 @@
 return {
     "yetone/avante.nvim",
     event = "VeryLazy",
-    version = false, -- Never set this value to "*"! Never!
+    version = false,
     opts = {
-        provider = "gemini_vertexai",
-        vendors = {
-            ---@type AvanteProvider
-            gemini_vertexai = {
-                endpoint = "https://us-central1-aiplatform.googleapis.com/v1/projects/danads-bit/locations/us-central1/publishers/google/models",
-                model = "gemini-1.5-flash-002",
-                api_key_name = 'Gemini key',
+        provider = "gemini",
+        vendor = {
+            vertex = {
+                parse_api_key = function ()
+                      local key_path = "/Users/mykyta.v.dotsenko/.config/avante.nvim/api_keys.json"
+                      local api_key = nil
 
-                parse_curl_args = function(opts, code_opts)
-                    local body_opts = vim.tbl_deep_extend("force", {}, {
-                        generationConfig = {
-                            temperature = 0,
-                            maxOutputTokens = 4096,
-                        },
-                    })
-                    return {
-                        url = opts.endpoint .. "/" .. opts.model .. ":streamGenerateContent?alt=sse",
-                        headers = {
-                            ["Authorization"] = "Bearer " .. opts.parse_api_key(opts.api_key_name),
-                            ["Content-Type"] = "application/json; charset=utf-8",
-                        },
-                        body = vim.tbl_deep_extend(
-                            "force",
-                            {},
-                            require("avante.providers.gemini").parse_messages(opts, code_opts),
-                            body_opts
-                        ),
-                    }
-                end,
-                parse_response = function(data_stream, event_state, opts)
-                    require("avante.providers.gemini").parse_response(data_stream, event_state, opts)
-                end,
+                      local ok, content = pcall(vim.fn.readfile, key_path)
+                      if ok and content then
+                          local ok_json, data = pcall(vim.fn.json_decode, table.concat(content, "\n"))
+                          if ok_json then
+                              api_key = data["GEMINI_API_KEY"]
+                          end
+                      end
+
+                      return api_key
+                end
             }
-        },
+        }
     },
     build = "make",
     dependencies = {
-    "nvim-treesitter/nvim-treesitter",
-    "stevearc/dressing.nvim",
-    "nvim-lua/plenary.nvim",
-    "MunifTanjim/nui.nvim",
-    -- Optional dependencies
-    "echasnovski/mini.pick",
-    "nvim-telescope/telescope.nvim",
-    "hrsh7th/nvim-cmp",
-    "ibhagwan/fzf-lua",
-    "nvim-tree/nvim-web-devicons",
-    "zbirenbaum/copilot.lua",
-    {
-      "HakonHarnes/img-clip.nvim",
-      event = "VeryLazy",
-      opts = {
-        default = {
-          embed_image_as_base64 = false,
-          prompt_for_file_name = false,
-          drag_and_drop = {
-            insert_mode = true,
-          },
-          use_absolute_path = true,
-        },
-      },
-    },
-    {
-      "MeanderingProgrammer/render-markdown.nvim",
-      opts = {
-        file_types = { "markdown", "Avante" },
-      },
-      ft = { "markdown", "Avante" },
-    },
-  },
+        "nvim-treesitter/nvim-treesitter",
+        "stevearc/dressing.nvim",
+        "nvim-lua/plenary.nvim",
+        "MunifTanjim/nui.nvim",
+        "echasnovski/mini.pick",
+        "nvim-telescope/telescope.nvim",
+        "hrsh7th/nvim-cmp",
+        "ibhagwan/fzf-lua",
+        "nvim-tree/nvim-web-devicons",
+        "zbirenbaum/copilot.lua",
+        {
+            "MeanderingProgrammer/render-markdown.nvim",
+            opts = {
+                file_types = { "markdown", "Avante" }
+            },
+            ft = { "markdown", "Avante" }
+        }
+    }
 }
